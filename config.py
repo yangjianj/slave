910e3eb13da79302a8db1138b0e5ef3f7ab7f952
yangjian <252089024@qq.com>
2019-10-25 15:27:16 +0800

    u

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 import os
 
-BASEDIR = os.path.dirname(os.path.abspath(__file__))
-LOG_FILE = os.path.join(BASEDIR,'log/system.log')
-REPORT_DIR = os.path.join(BASEDIR,'report/')
-#DATABASE = 'D:\yangjian\project\web_demo\django_web\db.sqlite3'
-DATABASE = 'F:\work\project\django_web\db.sqlite3'
 master_url = "http://127.0.0.1:8090/slave_heartbeat"
 masterip = '127.0.0.1'
 slaveip = '1.1.1.1'
 heartbeat = 10
 slave_label = 'test1'  #
 taskqueue = 'test1'
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+LOG_FILE = os.path.join(BASEDIR,'log/system.log')
+REPORT_DIR = os.path.join(BASEDIR,'report/')
+#DATABASE = 'D:\yangjian\project\web_demo\django_web\db.sqlite3'
+DATABASE = 'F:\work\project\django_web\db.sqlite3'
 
 #ui
 UICASE_DIR = os.path.join(BASEDIR,'cases/ui/')
