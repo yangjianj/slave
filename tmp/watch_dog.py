42d9f03d23bf0c2f76fdfe1d8cc43b64dca3297a
yangjianj <252089024@qq.com>
2020-06-02 17:44:20 +0800

    a

---
+++ tmp/watch_dog.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import sys
+import time
+import logging
+
+from watchdog.events import *
+from watchdog.observers import Observer
+
+
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        #FileSystemEventHandler.__init__(self)
+        super().__init__()
+
+    def on_moved(self, event):
+        if event.is_directory:
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),": directory moved from {0} to {1}".format(event.src_path, event.dest_path))
+        else:
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),"： file moved from {0} to {1}".format(event.src_path, event.dest_path))
+
+    def on_created(self, event):
+        if event.is_directory:
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),"： directory created:{0}".format(event.src_path))
+        else:
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),"： file created:{0}".format(event.src_path))
+
+    def on_deleted(self, event):
+        if event.is_directory:
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),"： directory deleted:{0}".format(event.src_path))
+        else:
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),"： file deleted:{0}".format(event.src_path))
+
+    def on_modified(self, event):
+        if event.is_directory:
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),"： directory modified:{0}".format(event.src_path))
+        else:
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),"： file modified:{0}".format(event.src_path))
+
+
+if __name__ == "__main__":
+    observer = Observer()
+    event_handler = FileEventHandler()
+    observer.schedule(event_handler, "e:/tmp", True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
